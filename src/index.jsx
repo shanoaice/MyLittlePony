0791876d82ac0951a9b6a334a524a64006e04809
KsRyY <andy200511@126.com>
2019-12-30 21:12:57 +0800

    :paintbrush: Use querySelector instead of getElementById

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-render(<App />, document.getElementById('app'))
+render(<App />, document.querySelector('#app'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
